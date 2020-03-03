0abf684191e17c0c2121c288a2babd0bf87cfd0d
Your Name <huynhpham765@gmail.com>
2020-03-03 14:00:57 +0700

    [DEV][Sign In] create feature sign in

---
+++ ServiceDo/app/src/main/java/com/example/servicedo/Pages/HomePage/Controller/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.servicedo.Pages.HomePage.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.example.servicedo.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tabs);
+    }
+}
---
+++ ServiceDo/app/src/main/java/com/example/servicedo/Pages/HomePage/Model/TabAdapter.java
@@ -0,0 +1,28 @@
+package com.example.servicedo.Pages.HomePage.Model;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabAdapter extends FragmentPagerAdapter {
+
+    public TabAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    public TabAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
